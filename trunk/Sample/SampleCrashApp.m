32d1369760b8170bffd87c1fef5c6d0018afe3d6
andre.pang@gmail.com <andre.pang@gmail.com@ae0330db-c83e-0410-b0b4-493f1c34c27a>
2007-11-13 07:15:20 +0000

    Make sure that the sample crasher application actually crashes on Leopard.

--- trunk/Sample/SampleCrashApp.m
+++ trunk/Sample/SampleCrashApp.m
@@ -12,6 +12,11 @@
 
 - (IBAction)doCrash:(id)sender
 {
+	// Leopard installs an extra exception handler that will catch the NULL-
+	// pointer dereference below, so we need to reset it to the default OS
+	// behaviour.
+	signal(SIGBUS, SIG_DFL);
+	
 	// Write to address 0 -> ka-boom!!!!
 	*(long*)0 = 0xDEADBEEF;
 }
